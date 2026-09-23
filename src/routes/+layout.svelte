28e6b2a134b1f5ee0138d7481d17db2e4abb0024
Gijs <gijs.nagtegaal@hva.nl>
2026-09-23 20:24:56 +0200

    BUILD: add all needed folders, routes and files for a functioning sveltekit website completed #50

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href="/src/lib/assets/styles/styleguide.css" />
+	<link rel="stylesheet" href="/src/lib/assets/styles/globals.css" />
 </svelte:head>
 
 {@render children()}
