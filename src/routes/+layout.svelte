1f65a1063591f40bd137297f90b8a38095de9556
Gijs <gijs.nagtegaal@hva.nl>
2026-09-24 19:57:51 +0200

    Finished mobile menu drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 <svelte:head>
     <link rel="icon" href={favicon} />
     <link rel="stylesheet" href="/stylesheet.css" />
-    <link rel="stylesheet" href="/styles/globals.css" />
+    <link rel="stylesheet" href="src/lib/assets/styles/globals.css" />
 </svelte:head>
 
 <Header />
